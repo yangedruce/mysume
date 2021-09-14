9c773bf6a03fe3224b7820846c2daa3f8db90ab8
Matt Daneshvar <matt.daneshvar@gmail.com>
2021-09-14 11:36:56 +0800

    Refactor UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,46 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Resume;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
-    // home
-    public function home(Request $request) {
-        $resume = Resume::where('user_id', auth()->user()->id)
-                        ->get();
-
-        return view('home')
-                ->with([
-                    'resumes' => $resume
-                ]);
+    public function home() {
+        return view('home', ['resumes' => auth()->user()->resumes]);
     }
 
-    // check username
-    public function checkUsername(Request $request) {
-        $user = User::where('username', $request->username)
-                    ->first();
+    public function usernameExists(Request $request) {
+        $query = User::where('username', $request->username);
 
-        if($user==null) {
-            return false;
-        }else {
-            return true;
+        if(auth()->check()){
+            $query->where('username', '!=', auth()->user()->username);
         }
+
+        return $query->first() !== null;
     }
-
-    // Check username profile
-    public function checkUsernameProfile(Request $request) {
-        $user = User::where('username', $request->username)
-                    ->where('username', '!=', auth()->user()->username)
-                    ->first();
-
-        if($user==null) {
-            return false;
-        }else {
-            return true;
-        }
-    } 
 }
