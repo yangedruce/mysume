e2e1939d58ef711d3c5759b9e446a8bf581af300
Yang Edruce <nurulizyansyaza@gmail.com>
2024-01-31 13:09:33 +0800

    fix: 419 page expired

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="{{ asset('assets/img') }}/favicon.ico" type="image/x-icon">
